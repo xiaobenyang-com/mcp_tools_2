93b3074f0af27878f955c489b2f2d34cb12d1542
appcode2022 <appcode2022@163.com>
2025-12-29 12:47:13 +0800

    add change

--- src/stdio.ts
+++ src/stdio.ts
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
 
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
-import { server } from './mcp.js';
+import { server, isLoading } from './mcp.js';
 
 try {
+  while (!isLoading) {
+    setTimeout(() => {
+      console.log('500 毫秒后执行');
+    }, 500);
+  }
+
+
   const transport = new StdioServerTransport();
   await server.connect(transport);
-  console.log(`Bazi MCP server is running on stdio.`);
+  console.log(`MCP server is running on stdio.`);
 } catch (error) {
-  console.error(`Failed to run Bazi MCP server on stdio.`);
+  console.error(`Failed to run MCP server on stdio.`);
 }
