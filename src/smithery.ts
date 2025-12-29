b621a6e31f444e626c4196037bb28eb06fc6b3c3
appcode2022 <appcode2022@163.com>
2025-12-29 17:07:28 +0800

    add change

--- src/smithery.ts
+++ src/smithery.ts
@@ -5,12 +5,18 @@ export const configSchema = z.object({
 })
 
 export default function createServer({config,}: { config: z.infer<typeof configSchema> }) {
+    let isLoading = true;
+    let curServer;
+    getServer().then(serverInstance => {
+        curServer = serverInstance.server;
+        isLoading = false;
+    });
 
-    while (state.isLoading) {
+    while (isLoading) {
         setTimeout(() => {
             console.log('500 毫秒后执行');
         }, 500);
     }
 
-    return getServer();
+    return curServer;
 }
